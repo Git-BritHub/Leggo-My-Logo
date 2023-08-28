3336d5fe53077161b73547618f0be3675dffe699
Brittany Brimley <brittanysapp@gmail.com>
2023-08-28 09:23:02 -0600

    working out functionality of how the shape will be set up before being created

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const inquirer = require("inquirer");
 const { writeFile } = require("fs/promises");
 const { Circle, Triangle, Square } = require("./lib/shapes");
-const svg = require("./lib/svg");
+const SVG = require("./lib/svg");
 
 // array of logo related questions for user input
 const questions = [
     {
         type: "input",
-        name: "logoText",
+        name: "text",
         message: "Enter up to three characters of text for your logo: ",
     },
     {
@@ -17,21 +17,31 @@ const questions = [
         message: "What would you like the color of your text to be? (Enter color name or hexadecimal number)",
     },
     {
-        type: "input",
+        type: "list",
         name: "shape",
-        message: "What shape would you like your color to be?",
-        choices: ["circle", "triangle", "square"],
+        message: "What shape would you like your svg to be?",
+        choices: ["Circle", "Triangle", "Square"],
     },
     {
         type: "input",
         name: "shapeColor",
         message: "What color would you like your shape to have? (Enter color name or hexadecimal number)",
-    }, 
+    },
 ];
 
 const initLogo = () => {
     inquirer.prompt(questions).then((data) => {
-        writeFile("logo.svg", { Circle, Triangle, Square }, svg(data))
+        let shape;
+        if(data.shape === "Circle") {
+            shape = new Circle()
+        } else if (data.shape === "Triangle") {
+            shape = new Triangle()
+        } else if (data.shape === "Square") {
+            shape = new Square()
+        }
+        shape.setColor(data.shapeColor)
+        
+        //writeFile("logo.svg", SVG(data))
     });
 };
 
